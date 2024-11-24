2eacf67d9f4ccaa183bd9884fa63e561565baeef
faizanalim <faizan.23@hotmail.com>
2024-11-25 02:31:09 +0500

    toDoList-reactapp

---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+export default function ({completedTodo,incompletedTodo})
+{
+
+    return(
+<div>
+    <h1>My ToDos</h1>
+<p>New Todos form will go here...Create New</p>
+    <h3>Complete:</h3>
+    <h3>Incomplete:</h3>
+</div>
+
+    )
+}
