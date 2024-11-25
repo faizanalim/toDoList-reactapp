06b1590b7b92660cbaf4a8999fb284d043a97dba
faizanalim <faizan.23@hotmail.com>
2024-11-25 10:15:03 +0500

    Prop Drilling between Components

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,29 @@ function App() {
   const [incompleteTodos, setIncompleteTodos] = useState([
     { text: 'Paint the house', isCompleted: false }
   ]);
+  function markTodoAsComplete(text) {
+    setIncompleteTodos(incompleteTodos.filter(t => t.text !== text));
+    setCompletedTodos([...completedTodos, { ...incompleteTodos.find(t => t.text === text), isCompleted: true }]);
+  }
 
+  function deleteTodo(text) {
+    setCompletedTodos(completedTodos.filter(t => t.text !== text));
+  }
+
+  function createTodo(text) {
+    setIncompleteTodos([...incompleteTodos, { text, isCompleted: false }]);
+  }
   return (
     <>
-      <TodoList completedTodos={completedTodos} incompleteTodos={incompleteTodos}></TodoList>
+      <TodoList
+        completedTodos={completedTodos}
+        incompleteTodos={incompleteTodos}
+        onCompletedClicked={markTodoAsComplete}
+        onDeleteClicked={deleteTodo}
+        onCreateClicked={createTodo}
+      />
     </>
+
   )
 }
 
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,21 @@
 import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./ToDoListItems"
 
-export default function TodoList({ completedTodos, incompleteTodos }) {
-    console.log(completedTodos);
+export default function TodoList({ completedTodos, incompleteTodos, onCompletedClicked, 
+                                  onDeleteClicked, onCreateClicked }) {
     return (
-        <div>
-            <h1>My Todos</h1>
-            <NewTodoForm />
-            <h3>Completed:</h3>
-            {completedTodos.map((todo, index) => (
-                <TodoListItem todo={todo} key={index} />
-            ))}
-            <h3>Incomplete:</h3>
-            {incompleteTodos.map((todo, index) => (
-                <TodoListItem todo={todo} key={index} />
-            ))}
-        </div>
+      <div>
+        <h1>My Todos</h1>
+        <NewTodoForm onCreateClicked={onCreateClicked} />
+        <h3>Completed:</h3>
+        {completedTodos.map((todo, index) => (
+          <TodoListItem todo={todo} key={index} onDeleteClicked={onDeleteClicked} />
+        ))}
+        <h3>Incomplete:</h3>
+        {incompleteTodos.map((todo, index) => (
+          <TodoListItem todo={todo} key={index} onCompletedClicked={onCompletedClicked} />
+        ))}
+      </div>
 
     )
 }
