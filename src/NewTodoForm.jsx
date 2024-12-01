914708be40038a14268b018fae4a96759b23c447
faizanalim <faizan.23@hotmail.com>
2024-12-01 07:26:10 +0500

    Adding Redux

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTodo } from "./todosSlice";
 
-export default function NewTodoForm({ onCreateClicked }) {
-    const [inputText, setInputText] = useState('');
+export default function NewTodoForm() {
+  const [inputText, setInputText] = useState("");
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <input type="text"
-                value={inputText}
-                onChange={e => setInputText(e.target.value)} />
-            <button onClick={() => {
-                onCreateClicked(inputText);
-                setInputText('');
-            }}>Create Todo</button>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          dispatch(createTodo(inputText));
+          setInputText("");
+        }}
+      >
+        Create Todo
+      </button>
+    </div>
+  );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,21 @@
 import NewTodoForm from "./NewTodoForm";
-import TodoListItem from "./ToDoListItems"
+import TodoListItem from "./ToDoListItems";
+import { useSelector } from "react-redux";
+export default function TodoList() {
+  const todos = useSelector((state) => state.todos.value);
 
-export default function TodoList({ completedTodos, incompleteTodos, onCompletedClicked, 
-                                  onDeleteClicked, onCreateClicked }) {
-    return (
-      <div>
-        <h1>My Todos</h1>
-        <NewTodoForm onCreateClicked={onCreateClicked} />
-        <h3>Completed:</h3>
-        {completedTodos.map((todo, index) => (
-          <TodoListItem todo={todo} key={index} onDeleteClicked={onDeleteClicked} />
-        ))}
-        <h3>Incomplete:</h3>
-        {incompleteTodos.map((todo, index) => (
-          <TodoListItem todo={todo} key={index} onCompletedClicked={onCompletedClicked} />
-        ))}
-      </div>
-
-    )
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <NewTodoForm />
+      <h3>Completed:</h3>
+      {todos.map((todo, index) => (
+        <TodoListItem todo={todo} key={index} />
+      ))}
+      <h3>Incomplete:</h3>
+      {todos.map((todo, index) => (
+        <TodoListItem todo={todo} key={index} />
+      ))}
+    </div>
+  );
 }
